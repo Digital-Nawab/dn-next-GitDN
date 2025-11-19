e86857bafe10f0c81164fb80521e5c61b9ba5cdd
shivam-yadav2 <shivsupertech@gmail.com>
2025-11-19 17:57:06 +0530

    button updated

--- src/components/Home/WhychooseUs.jsx
+++ src/components/Home/WhychooseUs.jsx
@@ -93,10 +93,10 @@ export default function WhyChooseUsSection() {
                   Get Started Now
                   <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform duration-300" />
                 </button> */}
-                <ContactPopup
+                {/* <ContactPopup
                   buttonText="Get Started Now"
                   buttonClassName=" cursor-pointer group bg-lime-400 z-50 hover:bg-lime-500 text-gray-900 font-semibold px-4 py-2 rounded-xl transition-all duration-300 flex items-center gap-3"
-                />
+                /> */}
               </WobbleCard>
               <WobbleCard containerClassName="col-span-1 z-30 lg:col-span-3 bg-blue-900  xl:min-h-[600px] xl:min-h-[300px]">
                 <div className="group bg-transparent rounded-2xl p-4 shadow-sm hover:shadow-xl transition-all duration-300 lg:col-span-3 transform hover:-translate-y-1">
